73466de7a81ac7c162c5867101be8945a6369266
comaiivilvert <comaiivilvert@gmail.com>
2025-03-20 20:25:11 -0300

    geração do numero aleatorio para adivinhação.

--- JogoDeAdicinhacaoConsoleApp/Program.cs
+++ JogoDeAdicinhacaoConsoleApp/Program.cs
@@ -3,17 +3,26 @@
     internal class Program
     {
 
-        //Versão 1: Estrutura básica e entrada do usuário 
+        //Versão 2: Gerar um número secreto aleatório 
         static void Main(string[] args)
         {
             Console.WriteLine("----------------------");
             Console.WriteLine("Jogo de Adivinhação");
             Console.WriteLine("----------------------");
 
-            Console.Write("Digite um numero para chutar:");
+            //logica do jogo
+            Random geradorDeNumeros = new Random();
+
+            int numeroSecreto = geradorDeNumeros.Next(1,21);
+
+
+
+            Console.Write("Digite um numero (de 1 a 20) para chutar:");
             int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("O numero digitado foi:" + numeroDigitado);
-            
+            Console.WriteLine("O numero secreto é:" + numeroSecreto);
+
             Console.ReadLine();
         }
     }
