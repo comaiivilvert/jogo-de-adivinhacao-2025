928517b5658b2328a851a056e2512343e20f06a0
comaiivilvert <comaiivilvert@gmail.com>
2025-03-20 20:07:21 -0300

    Implementado a entrada de dados do usuario

--- JogoDeAdicinhacaoConsoleApp/Program.cs
+++ JogoDeAdicinhacaoConsoleApp/Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
+
+        //Versão 1: Estrutura básica e entrada do usuário 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("----------------------");
+
+            Console.Write("Digite um numero para chutar:");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("O numero digitado foi:" + numeroDigitado);
+            
+            Console.ReadLine();
         }
     }
 }
